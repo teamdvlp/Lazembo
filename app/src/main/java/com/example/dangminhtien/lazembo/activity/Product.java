641eda3372a66fba47197aae2e0597af8f48d752
dangminhtien <tiendvlp@gmail.com>
2017-06-01 22:43:58 +0700

    test

--- app/src/main/java/com/example/dangminhtien/lazembo/activity/Product.java
+++ app/src/main/java/com/example/dangminhtien/lazembo/activity/Product.java
@@ -14,6 +14,6 @@ public class Product extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
-
+        //aa
     }
 }
